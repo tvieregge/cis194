f505d070e87c76a3319268ec0a2366d86f6bd2e6
tvieregge <tmvieregge@gmail.com>
2019-01-28 16:53:23 -0500

    using hspec

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,13 +1,29 @@
-import Lib
-import Log
+import           Lib
+import           Log
+import           Test.Hspec
 
 main :: IO ()
-main = do
-    putStrLn . show $ hanoi 3 "a" "b" "c"
-    putStrLn . show $ parseMessage "ignored"
-    putStrLn . show $ parseMessage "E 1 1 I'm an error"
-    putStrLn . show $ parseMessage "W  1 I'm a warn"
-    putStrLn . show $ parseMessage "I  1 I'm an info"
-    log <- testParse parse 5 "test/error.log"
-    putStrLn $ show log
-    putStrLn . show $ foldl (\acc x -> insert x acc) Leaf log
+main = hspec $ do
+    describe "hanoi" $ do
+        it "can solve the tower of hanoi problem" $ do
+            let ans = [("a","c"),("a","b"),("c","b"),("a","c"),("b","a"),("b","c"),("a","c")]
+            hanoi 3 "a" "b" "c" `shouldBe` ans
+    describe "log parsing" $ do
+        it "can ignore (some) bad input" $ do
+            parseMessage "ignored" `shouldBe` Unknown "ignored"
+        it "can parse all message types" $ do
+            parseMessage "E 1 1 I'm an error" `shouldBe` LogMessage (Error 1) 1 "I'm an error"
+            parseMessage "W  1 I'm a warn" `shouldBe` LogMessage Warning 1 "I'm a warn"
+            parseMessage "I  1 I'm an info" `shouldBe` LogMessage Info 1 "I'm an info"
+        it "can read an input file" $ do
+        -- how do I test IO without a huge variable?
+            let ans = [LogMessage Info 6 "6",LogMessage Info 1 "1",LogMessage Info 4 "4",
+                       LogMessage Info 11 "11",LogMessage (Error 70) 70 ""]
+            testParse parse 5 "test/sample.log" `shouldReturn` ans
+        it "can build a BST" $ do
+            log <- testParse parse 5 "test/sample.log"
+            let ans = Node (Node Leaf (LogMessage Info 1 "1") (Node Leaf
+                    (LogMessage Info 4 "4") Leaf)) (LogMessage Info 6 "6")
+                    (Node Leaf (LogMessage Info 11 "11") (Node Leaf
+                    (LogMessage (Error 70) 70 "") Leaf))
+            foldl (\acc x -> insert x acc) Leaf log `shouldBe` ans
