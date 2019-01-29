73fc0e9f29778db2e2b55f338a12e92b1114a1b0
tvieregge <tmvieregge@gmail.com>
2019-01-28 19:46:54 -0500

    formatting test code

--- test/Spec.hs
+++ test/Spec.hs
@@ -8,18 +8,22 @@ main = hspec $ do
         it "can solve the tower of hanoi problem" $ do
             let ans = [("a","c"),("a","b"),("c","b"),("a","c"),("b","a"),("b","c"),("a","c")]
             hanoi 3 "a" "b" "c" `shouldBe` ans
+
     describe "log parsing" $ do
         it "can ignore (some) bad input" $ do
             parseMessage "ignored" `shouldBe` Unknown "ignored"
+
         it "can parse all message types" $ do
             parseMessage "E 1 1 I'm an error" `shouldBe` LogMessage (Error 1) 1 "I'm an error"
             parseMessage "W  1 I'm a warn" `shouldBe` LogMessage Warning 1 "I'm a warn"
             parseMessage "I  1 I'm an info" `shouldBe` LogMessage Info 1 "I'm an info"
+
         it "can read an input file" $ do
         -- how do I test IO without a huge variable?
             let ans = [LogMessage Info 6 "6",LogMessage Info 1 "1",LogMessage Info 4 "4",
                        LogMessage Info 11 "11",LogMessage (Error 70) 70 ""]
             testParse parse 5 "test/sample.log" `shouldReturn` ans
+
         it "can build a BST" $ do
             log <- testParse parse 5 "test/sample.log"
             let ans = Node (Node Leaf (LogMessage Info 1 "1") (Node Leaf
