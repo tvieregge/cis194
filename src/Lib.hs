d2fb732fab09094198dccbcd91b58e09f8437321
tvieregge <tmvieregge@gmail.com>
2019-01-31 19:10:36 -0500

    week 6, steam complete

--- src/Lib.hs
+++ src/Lib.hs
@@ -23,6 +23,13 @@ import ExprT
 import Log
 import Parser
 
+data Stream a =
+    Cons a
+         (Stream a)
+
+instance Show a => Show (Stream a) where
+    show a = concat . map (++ " , ") . map show . take 20 $ streamToList a
+
 class Expr a where
     lit :: Integer -> a
     add :: a -> a -> a
@@ -158,16 +165,40 @@ evalStr s =
 fib :: Integer -> Integer
 fib 0 = 0
 fib 1 = 1
-fib n = fib (n-1) + fib (n-2)
+fib n = fib (n - 1) + fib (n - 2)
 
 fibs1 :: [Integer]
 fibs1 = map fib [0 ..]
 
 fib2 :: Integer -> Integer
 fib2 n = go 0 1 n
-    where go prev _ 0 = prev
-          go prev prev2 n = go (prev + prev2) prev  (n-1)
+  where
+    go prev _ 0 = prev
+    go prev prev2 n = go (prev + prev2) prev (n - 1)
 
 fibs2 :: [Integer]
 fibs2 = 0 : 1 : next fibs2
-    where next (a : rest@(b:_)) = a + b : next rest
+  where
+    next (a:rest@(b:_)) = a + b : next rest
+
+streamToList :: Stream a -> [a]
+streamToList (Cons a b) = a : streamToList b
+
+streamRepeat :: a -> Stream a
+streamRepeat a = Cons a $ streamRepeat a
+
+streamMap :: (a -> b) -> Stream a -> Stream b
+streamMap f (Cons x y) = Cons (f x) $ streamMap f y
+
+streamFromSeed :: (a -> a) -> a -> Stream a
+streamFromSeed f s = Cons (f s) $ streamFromSeed f (f s)
+
+nats :: Stream Integer
+nats = streamFromSeed (+ 1) 0
+
+interleaveStreams :: Stream a -> Stream a -> Stream a
+interleaveStreams (Cons a1 a2) b =
+    Cons a1 $ interleaveStreams b a2
+
+ruler :: Stream Integer
+ruler = interleaveStreams (streamRepeat 0) $ streamMap (+ 1) ruler
