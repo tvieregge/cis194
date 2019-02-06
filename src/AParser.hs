ad0c074a5305d215adbe8c65fcf7a8ae52ba5c71
tvieregge <tmvieregge@gmail.com>
2019-02-05 21:15:18 -0500

    week 8 ex 2, also formatting AParser.hs

--- src/AParser.hs
+++ src/AParser.hs
@@ -1,18 +1,19 @@
 {- CIS 194 HW 10
    due Monday, 1 April
 -}
-
 module AParser where
 
-import           Control.Applicative
+import Control.Applicative
 
-import           Data.Char
+import Data.Char
 
 -- A parser for a value of type a is a function which takes a String
 -- represnting the input to be parsed, and succeeds or fails; if it
 -- succeeds, it returns the parsed value along with the remainder of
 -- the input.
-newtype Parser a = Parser { runParser :: String -> Maybe (a, String) }
+newtype Parser a = Parser
+    { runParser :: String -> Maybe (a, String)
+    }
 
 -- For example, 'satisfy' takes a predicate on Char, and constructs a
 -- parser which succeeds only if it sees a Char that satisfies the
@@ -21,12 +22,12 @@ newtype Parser a = Parser { runParser :: String -> Maybe (a, String) }
 satisfy :: (Char -> Bool) -> Parser Char
 satisfy p = Parser f
   where
-    f [] = Nothing    -- fail on the empty input
-    f (x:xs)          -- check if x satisfies the predicate
+    f [] = Nothing -- fail on the empty input
+    f (x:xs) -- check if x satisfies the predicate
                         -- if so, return x along with the remainder
                         -- of the input (that is, xs)
-        | p x       = Just (x, xs)
-        | otherwise = Nothing  -- otherwise, fail
+        | p x = Just (x, xs)
+        | otherwise = Nothing -- otherwise, fail
 
 -- Using satisfy, we can define the parser 'char c' which expects to
 -- see exactly the character c, and fails otherwise.
@@ -43,24 +44,32 @@ Nothing
 Just ('x',"yz")
 
 -}
-
 -- For convenience, we've also provided a parser for positive
 -- integers.
 posInt :: Parser Integer
 posInt = Parser f
   where
     f xs
-      | null ns   = Nothing
-      | otherwise = Just (read ns, rest)
-      where (ns, rest) = span isDigit xs
+        | null ns = Nothing
+        | otherwise = Just (read ns, rest)
+      where
+        (ns, rest) = span isDigit xs
 
 ------------------------------------------------------------
 -- Your code goes below here
 ------------------------------------------------------------
-
--- fmap :: (a -> b) -> f a -> f b
 instance Functor Parser where
     fmap f (Parser pf) = Parser (fmap (fmap (first f)) pf)
 
-first :: (a -> b) -> (a,c) -> (b,c)
-first f (x,y) = (f x, y)
+first :: (a -> b) -> (a, c) -> (b, c)
+first f (x, y) = (f x, y)
+
+instance Applicative Parser where
+    pure a = Parser (\s -> Just (a, s))
+    -- (<*>) :: (Applicative Parser) => Parser (a -> b) -> Parser a -> Parser b
+    (Parser pf) <*> xp =
+        Parser
+            (\str ->
+                 case pf str of
+                     Nothing -> Nothing
+                     Just (f, str') -> runParser (f <$> xp) str')
