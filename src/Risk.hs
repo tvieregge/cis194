55000f76bb0d6f619493e5d9a242ccb002dac587
tvieregge <tmvieregge@gmail.com>
2019-02-07 00:21:11 -0500

    week 12 ex 2

--- src/Risk.hs
+++ src/Risk.hs
@@ -3,6 +3,7 @@
 module Risk where
 
 import Control.Monad.Random
+import Data.List
 
 ------------------------------------------------------------
 -- Die values
@@ -28,3 +29,26 @@ data Battlefield = Battlefield
     { attackers :: Army
     , defenders :: Army
     }
+
+rollN :: Int -> Rand StdGen [DieValue]
+rollN 0 = return []
+rollN n = do
+    roll <- die
+    next <- rollN (n - 1)
+    return (roll : next)
+
+battleResults :: (DieValue, DieValue) -> (Army, Army) -> (Army, Army)
+battleResults (a, d) (aAcc, dAcc)
+    | diff > 0 = (aAcc, dAcc - 1)
+    | otherwise = (aAcc - 1, dAcc)
+  where
+    diff = unDV a - unDV d
+
+battle :: Battlefield -> Rand StdGen Battlefield
+battle (Battlefield att def) = do
+    attRoll <- roll 3 att
+    defRoll <- roll 2 def
+    let (a, d) = foldr battleResults (0, 0) $ zip attRoll defRoll
+    return (Battlefield (att-a) (def-d))
+  where
+    roll armySize army = (sortBy (flip compare)) <$> (rollN $ min armySize army)
